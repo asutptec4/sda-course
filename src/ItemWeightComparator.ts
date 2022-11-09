f80006192ee6f93fdb8299811d0c597cded078af
Aliaksandr Shyshonak <asutp_tec4@tut.by>
2022-11-09 14:00:25 +0300

    fix: resolve comments

--- src/ItemWeightComparator.ts
+++ src/ItemWeightComparator.ts
@@ -3,11 +3,10 @@ import { ItemComparator } from './ItemComparator';
 
 export class ItemWeightComparator implements ItemComparator {
   compare(first: Item, second: Item): number {
-    const diff = first.getWeight() - second.getWeight();
-    if (diff > 0) {
+    if (first.getWeight() > second.getWeight()) {
       return 1;
     }
-    if (diff < 0) {
+    if (first.getWeight() < second.getWeight()) {
       return -1;
     }
     return first.compareTo(second);
